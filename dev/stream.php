32804798df088c66ada5ee6b16d1c47929ae352d
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-01-15 11:15:28 -0300

    react php - stream simulando captura de erro durante uma stream pipe

--- dev/stream.php
+++ dev/stream.php
@@ -1,5 +1,6 @@
 <?php
 
+use JetBrains\PhpStorm\NoReturn;
 use React\EventLoop\Loop;
 use React\Stream\ReadableResourceStream;
 use React\Stream\ThroughStream;
@@ -7,19 +8,30 @@ use React\Stream\WritableResourceStream;
 
 require __DIR__ . '/vendor/autoload.php';
 
+#[NoReturn]
+function error(string $message): void
+{
+    dd($message);
+}
+
 $loop = Loop::get();
 
 $readable = new ReadableResourceStream(STDIN, $loop);
 $writable = new WritableResourceStream(STDOUT, $loop);
+$error = new WritableResourceStream(STDERR, $loop);
 
 $toUpper = new ThroughStream(fn ($chunk) => strtoupper($chunk));
 $toStringReverse = new ThroughStream(fn ($chunk) => strrev($chunk));
 $toSnakeCase = new ThroughStream(fn ($chunk) => str_replace(' ', '_', strtolower($chunk)));
+$exampleError = new ThroughStream(fn ($chunk) => throw new Exception('Example error'));
+
+$exampleError->on('error', fn (Exception $error) => error($error->getMessage()));
 
 $readable
     ->pipe($toUpper)
     ->pipe($toStringReverse)
     ->pipe($toSnakeCase)
+    ->pipe($exampleError)
     ->pipe($writable);
 
 $loop->run();
