be6a8f8be9cd01331617699c16468c41a66aac9f
Marcelo Fabiano <marcelofabianov@gmail.com>
2024-01-15 11:06:54 -0300

    react php - stream inicio

---
+++ dev/stream.php
@@ -0,0 +1,30 @@
+<?php
+
+use React\EventLoop\Loop;
+use React\Stream\ReadableResourceStream;
+use React\Stream\ThroughStream;
+use React\Stream\WritableResourceStream;
+
+require __DIR__ . '/vendor/autoload.php';
+
+$loop = Loop::get();
+
+$readable = new ReadableResourceStream(STDIN, $loop);
+$writable = new WritableResourceStream(STDOUT, $loop);
+
+$toUpper = new ThroughStream(fn ($chunk) => strtoupper($chunk));
+$toStringReverse = new ThroughStream(fn ($chunk) => strrev($chunk));
+$toSnakeCase = new ThroughStream(fn ($chunk) => str_replace(' ', '_', strtolower($chunk)));
+
+$readable
+    ->pipe($toUpper)
+    ->pipe($toStringReverse)
+    ->pipe($toSnakeCase)
+    ->pipe($writable);
+
+$loop->run();
+
+
+/**
+ * echo "hello World" | php stream.php
+ */
